72ac21f83bcdf663ee2224157d2a42b0b184132c
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2022-11-05 22:10:43 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    <div className="App-Header">
+      <div>Inicio</div>
+      <div>Rotas</div>
+      <div>Fale Comigo!</div>
+      <div>Inicio</div>
+    </div>
+    <div className="App-Content">
+      Content
+    </div>
+  </div>;
+}
+
+export default App;
