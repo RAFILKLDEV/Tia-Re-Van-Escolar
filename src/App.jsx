82c223d84fe6f0f578da05181e9e73509f4193a8
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2022-11-06 20:09:22 -0300

    html Content updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    <div className="App-Header">
-      <div>Inicio</div>
-      <div>Rotas</div>
-      <div>Fale Comigo!</div>
-      <div>Inicio</div>
+  return (
+    <div className="App">
+      <div className="App-Header">
+        <div>Inicio</div>
+        <div>Rotas</div>
+        <div>Fale Comigo!</div>
+        <div>Inicio</div>
+      </div>
+      <div className="App-Content">
+        Content Atualizado
+      </div>
     </div>
-    <div className="App-Content">
-      Content
-    </div>
-  </div>;
+  );
 }
 
 export default App;
