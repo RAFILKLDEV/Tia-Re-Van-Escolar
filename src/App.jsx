dbba1ac2d3291d61e3fd7597ec28ffe24c29cf54
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2022-11-07 12:38:21 -0300

    Html updated using Material UI. Implemented TypeScript

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-Header">
-        <div>Inicio</div>
-        <div>Rotas</div>
-        <div>Fale Comigo!</div>
-        <div>Inicio</div>
-      </div>
-      <div className="App-Content">
-        Content Atualizado
-      </div>
+      <Box
+        sx={{
+          p: 2,
+          width: 800,
+          height: 25,
+          backgroundColor: "#FFE45C",
+          display: "flex",
+          gap: 5,
+          justifyContent: "center",
+          alignItems: "center",
+          margin: 15,
+        }}
+      >
+        <Button
+          sx={{
+            height: 30,
+          }}
+        >
+          Inicio
+        </Button>
+        <Button>Rotas</Button>
+        <Button>Fale Comigo!</Button>
+      </Box>
+      <div className="App-Content">Content Atualizado</div>
     </div>
   );
 }
